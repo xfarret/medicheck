760afa470da9a4121705013ebfe6f4c6fb95838a
xfarret <xavier.farret@kolor.com>
2014-05-04 20:10:53 +0200

    ajout de l'entity user et role

---
+++ src/Medicheck/UserBundle/Entity/RoleRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xavier
+ * Date: 04/05/14
+ * Time: 19:16
+ */
+
+namespace Medicheck\UserBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+class RoleRepository extends EntityRepository {
+
+} 
--- src/Medicheck/UserBundle/Entity/User.php
+++ src/Medicheck/UserBundle/Entity/User.php
@@ -2,7 +2,10 @@
 
 namespace Medicheck\UserBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+use Serializable;
 
 /**
  * User
@@ -10,7 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table()
  * @ORM\Entity(repositoryClass="Medicheck\UserBundle\Entity\UserRepository")
  */
-class User
+class User implements AdvancedUserInterface, Serializable
 {
     /**
      * @var integer
@@ -23,23 +26,126 @@ class User
 
     /**
      * @var string
+     */
+    private $firstname;
+
+    /**
+     * @var string
+     */
+    private $lastname;
+
+    /**
+     * @var string
+     */
+    private $salt;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+    /**
+     * @var string
+     */
+    private $passwordUnencoded;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var boolean
+     */
+    private $isActive;
+
+    /**
+     * @var boolean
+     */
+    private $locked;
+
+    /**
+     * @var string
+     */
+    private $resettingPasswordToken;
+
+    /**
+     * @var string
      *
      * @ORM\Column(name="numSecu", type="string", length=255)
      */
     private $numSecu;
 
     /**
-     * @var array
-     *
-     * @ORM\Column(name="beneficiaires", type="array")
+     * @var boolean
      */
-    private $beneficiaires;
-
+    private $isChild;
 
     /**
-     * Get id
-     *
-     * @return integer 
+     * @ORM\OneToMany(targetEntity="User", mappedBy="relatedTo")
+     */
+    private $recipients;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Role")
+     */
+    private $roles;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="recipients")
+     * @ORM\JoinColumn(name="related_id", referencedColumnName="id")
+     */
+    private $relatedTo;
+
+    public function __construct() {
+        $this->recipients = new ArrayCollection();
+        $this->isChild = false;
+        $this->passwordUnencoded = null;
+        $this->locked = false;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $firstname
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
      */
     public function getId()
     {
@@ -47,22 +153,79 @@ class User
     }
 
     /**
-     * Set numSecu
-     *
+     * @param boolean $isActive
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * @param boolean $isChild
+     */
+    public function setIsChild($isChild)
+    {
+        $this->isChild = $isChild;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getIsChild()
+    {
+        return $this->isChild;
+    }
+
+    /**
+     * @param string $lastname
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param boolean $locked
+     */
+    public function setLocked($locked)
+    {
+        $this->locked = $locked;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getLocked()
+    {
+        return $this->locked;
+    }
+
+    /**
      * @param string $numSecu
-     * @return User
      */
     public function setNumSecu($numSecu)
     {
         $this->numSecu = $numSecu;
-
-        return $this;
     }
 
     /**
-     * Get numSecu
-     *
-     * @return string 
+     * @return string
      */
     public function getNumSecu()
     {
@@ -70,25 +233,221 @@ class User
     }
 
     /**
-     * Set beneficiaires
-     *
-     * @param array $beneficiaires
-     * @return User
+     * @param string $passwordUnencoded
      */
-    public function setBeneficiaires($beneficiaires)
+    public function setPasswordUnencoded($passwordUnencoded)
     {
-        $this->beneficiaires = $beneficiaires;
-
-        return $this;
+        $this->passwordUnencoded = $passwordUnencoded;
     }
 
     /**
-     * Get beneficiaires
-     *
-     * @return array 
+     * @return string
      */
-    public function getBeneficiaires()
+    public function getPasswordUnencoded()
     {
-        return $this->beneficiaires;
+        return $this->passwordUnencoded;
+    }
+
+    /**
+     * @param mixed $recipients
+     */
+    public function setRecipients($recipients)
+    {
+        $this->recipients = $recipients;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRecipients()
+    {
+        return $this->recipients;
+    }
+
+    /**
+     * @param mixed $relatedTo
+     */
+    public function setRelatedTo($relatedTo)
+    {
+        $this->relatedTo = $relatedTo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRelatedTo()
+    {
+        return $this->relatedTo;
+    }
+
+    /**
+     * @param string $resettingPasswordToken
+     */
+    public function setResettingPasswordToken($resettingPasswordToken)
+    {
+        $this->resettingPasswordToken = $resettingPasswordToken;
+    }
+
+    /**
+     * @return string
+     */
+    public function getResettingPasswordToken()
+    {
+        return $this->resettingPasswordToken;
+    }
+
+
+    /**
+     * Checks whether the user's account has expired.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw an AccountExpiredException and prevent login.
+     *
+     * @return bool    true if the user's account is non expired, false otherwise
+     *
+     * @see AccountExpiredException
+     */
+    public function isAccountNonExpired()
+    {
+        return true;
+    }
+
+    /**
+     * Checks whether the user is locked.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a LockedException and prevent login.
+     *
+     * @return bool    true if the user is not locked, false otherwise
+     *
+     * @see LockedException
+     */
+    public function isAccountNonLocked()
+    {
+        return !($this->locked == true);
+    }
+
+    /**
+     * Checks whether the user's credentials (password) has expired.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a CredentialsExpiredException and prevent login.
+     *
+     * @return bool    true if the user's credentials are non expired, false otherwise
+     *
+     * @see CredentialsExpiredException
+     */
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    /**
+     * Checks whether the user is enabled.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a DisabledException and prevent login.
+     *
+     * @return bool    true if the user is enabled, false otherwise
+     *
+     * @see DisabledException
+     */
+    public function isEnabled()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     * <code>
+     * public function getRoles()
+     * {
+     *     return array('ROLE_USER');
+     * }
+     * </code>
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return Role[] The user roles
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * Returns the password used to authenticate the user.
+     *
+     * This should be the encoded password. On authentication, a plain-text
+     * password will be salted, encoded, and then compared to this value.
+     *
+     * @return string The password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        return $this->numSecu;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+    }
+
+    /**
+     * Serializes the content of the current User object
+     * @return string
+     */
+    public function serialize()
+    {
+        return \json_encode(
+            array(
+                $this->numSecu,
+                $this->roles,
+                $this->id
+            )
+        );
+    }
+
+    /**
+     * Unserializes the given string in the current User object
+     * @param serialized
+     */
+    public function unserialize($serialized)
+    {
+        list(
+            $this->numSecu,
+            $this->roles,
+            $this->id
+            ) = \json_decode($serialized);
     }
 }
