35d2a15ce8ac9863594cd3d7057125fb6e7248e3
Xavier Farret <xavier.farret@kolor.com>
2014-05-26 14:58:47 +0200

    finalisation de l'affichage dynamique d'ajout de récipients

--- src/Medicheck/CmsBundle/Resources/public/js/fn_account.js
+++ src/Medicheck/CmsBundle/Resources/public/js/fn_account.js
@@ -1,23 +1,26 @@
 jQuery(document).ready(function() {
     // Récupère le div qui contient la collection de tags
-    var collectionHolder = jQuery('table.recipients');
+    collectionHolder = jQuery('table.recipients');
 
     // ajoute un lien « add a tag »
-    var $addRecipientLink = jQuery('<a href="#" class="add_recipient_link">Ajouter un récipient</a>');
+    $addRecipientLink = jQuery('<a href="#" class="add_recipient_link">Ajouter un récipient</a>');
 
     // ajoute l'ancre « ajouter un tag » et li à la balise ul
-    collectionHolder.append($addRecipientLink);
+    collectionHolder.after($addRecipientLink);
 
     $addRecipientLink.on('click', function(e) {
         // empêche le lien de créer un « # » dans l'URL
         e.preventDefault();
 
         // ajoute un nouveau formulaire tag (voir le prochain bloc de code)
-        addRecipientForm(collectionHolder, $addRecipientLink);
+        addRecipientForm(collectionHolder);
     });
 });
 
-function addRecipientForm(collectionHolder, $newLinkLi) {
+var collectionHolder = null;
+var $addRecipientLink = null;
+
+function addRecipientForm(collectionHolder) {
     // Récupère l'élément ayant l'attribut data-prototype comme expliqué plus tôt
     var prototype = collectionHolder.attr('data-prototype');
 
@@ -29,5 +32,26 @@ function addRecipientForm(collectionHolder, $newLinkLi) {
     $newForm.find(':input').addClass('form-control');
 
     // Affiche le formulaire dans la page dans un li, avant le lien "ajouter un tag"
-    $newLinkLi.before($newForm);
+    collectionHolder.find('tbody').append($newForm);
+}
+
+function addRecipientRow() {
+    // Récupère l'élément ayant l'attribut data-prototype comme expliqué plus tôt
+    var prototype = collectionHolder.attr('row-prototype');
+
+    var $addRecipientDom = jQuery("#new-recipient");
+    var $newRecipient = $addRecipientDom;
+    $newRecipient.find("td").each(function(index) {
+        $this = jQuery( this );
+        $input = $this.children('input:first');
+        value = $input.val();
+        $this.empty();
+        $this.append(value);
+    });
+    $newRecipient.removeAttr('id');
+
+    // ajout de la nouvelle entrée
+    collectionHolder.find('tbody').children(':last').after($addRecipientDom);
+
+    return false;
 }
